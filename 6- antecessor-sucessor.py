5ca882370223b501865ed88d3c332a2ec2e819fb
rayan-ezequiel <rayan.ezequiel@gmail.com>
2026-08-21 16:44:54 -0300

    Add files via upload

---
+++ 6- antecessor-sucessor.py
@@ -0,0 +1,11 @@
+import os 
+os.system('cls')
+
+print('= Antecessor e Sucessor =')
+
+numero1 = int(input('Digite um numero: '))
+Antecessor = (numero1 - 1)
+Sucessor = (numero1 + 1)
+
+print("O antecessor é: ", Antecessor)
+print("O sucessor é: ", Sucessor)
