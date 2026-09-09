3585ec1af616fb78b4d913de10a5950a21aa8ab4
rayan-ezequiel <rayan.ezequiel@gmail.com>
2026-09-09 16:40:44 -0300

    Add files via upload

---
+++ 0030b-calendario-math-case.py
@@ -0,0 +1,26 @@
+import os
+os.system('cls')
+
+dia = int(input('Considere que Domingo é o dia 1 e Sábado é o dia 7\nDigite qual é o dia da semana: '))
+
+
+match dia:
+    case 1:
+        print('Hoje é Domingo, final de semana')
+    case 2:
+        print('Hoje é Segunda-Feira')
+    case 3:
+        print('Hoje é Terça-Feira')
+    case 4:
+        print('Hoje é Quarta-Feira')
+    case 5:
+        print('Hoje é Quinta-Feira')
+    case 6:
+        print('Hoje é Sexta-Feira')
+    case 7:
+        print('Hoje é Sábado, final de semana')
+if dia >= 2 and dia <=6:
+    print('Dia útil')
+if dia >= 8:
+    print('Dia inválido.')
+
